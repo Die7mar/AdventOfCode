760b81450469661b90b532de7159daa155af1f35
Dietmar M <die7mar@mailbox.org>
2022-12-01 15:40:13 +0100

    DayOne

---
+++ AdventOfCode/Challenges/DayOne.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Challenges
+{
+    internal class DayOne : IDayChallenge
+    {
+
+        public void Part1()
+        {
+            List<int> elfen = GetElfenSum();
+
+            var result = elfen.OrderByDescending(x => x).ToList()[0];
+            Console.WriteLine("Result Part 1 " + result);
+        }
+
+        public void Part2()
+        {
+            List<int> elfen = GetElfenSum();
+
+            var sortedList = elfen.OrderByDescending(x => x).ToList();
+            var result = sortedList[0] + sortedList[1] + sortedList[2];
+            Console.WriteLine("Result Part 2 " + result);
+        }
+
+        public List<int> GetElfenSum()
+        {
+            var content = File.ReadAllLines("Inputs\\day1.txt");
+            List<int> elfen = new List<int>();
+            var currentElf = 0;
+
+            foreach (var row in content)
+            {
+                if (string.IsNullOrEmpty(row))
+                {
+                    elfen.Add(currentElf);
+                    currentElf = 0;
+                }
+                else { currentElf += Convert.ToInt32(row); }
+            }
+
+            return elfen.OrderByDescending(x => x).ToList();
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,32 @@
+﻿using AdventOfCode;
+using AdventOfCode.Challenges;
+
+
+while (true)
+{
+    Console.WriteLine("Enter the Date or type c for cancel");
+    string input = Console.ReadLine();
+
+    if (input.ToLower() == "c") { break; }
+
+    bool isNumber = int.TryParse(input, out int selectedDate);
+
+    if (!isNumber) { Console.WriteLine("Please tippe a valid number!"); }
+
+    IDayChallenge challenge = null;
+
+    switch (selectedDate)
+    {
+        case 1:
+            challenge = new DayOne();
+            break;
+        default:
+            Console.WriteLine("No DATA");
+            continue;
+    }
+
+    challenge.Part1();
+    challenge.Part2();
+}
+
+
