18b6332b59aef90e80614e753d07add50679e484
Dietmar M <die7mar@mailbox.org>
2022-12-05 23:03:05 +0100

    Complete day 5

--- AdventOfCode/Challenges/DayFive.cs
+++ AdventOfCode/Challenges/DayFive.cs
@@ -10,80 +10,113 @@ namespace AdventOfCode.Challenges
     internal class DayFive : IDayChallenge
     {
         const int CountStacks = 9;
-        public void Part1()
-        {
-            //throw new NotImplementedException();
-            var readed = Read();
+        public void Part1() => MoveIt(Mover.CreateMover9000);
+       
+        public void Part2() => MoveIt(Mover.CreateMover9001);
 
-            var content = File.ReadAllLines("Inputs\\day5.txt");
-            for (int i = readed.startOnGoing; i < content.Length; i++)
+        private void MoveIt(Mover mover)
+        {
+
+            var readedInput = ReadFile();
+            var containers = ReadContainer(readedInput.containers);
+
+            switch (mover)
             {
-                Move(ref readed.stacks, content[i]);
+                case Mover.CreateMover9000:
+                    readedInput.cmd.ToList().ForEach(x => CreateMover9000(ref containers, x));
+                    break;
+                case Mover.CreateMover9001:
+                    readedInput.cmd.ToList().ForEach(x => CreateMover9001(ref containers, x));
+                    break;
             }
 
             string resultString = "";
 
-            for (int i = 0; i < CountStacks; i++)
-            {
-                resultString += readed.stacks[i][0];
-            }
+            for (int i = 0; i < CountStacks; i++) { resultString += containers[i].Last(); }
 
             Console.WriteLine("Result: " + resultString);
         }
 
 
-        private (List<List<char>> stacks, int startOnGoing) Read()
+        private (string[] containers, string[] cmd) ReadFile()
+        {
+            string content = File.ReadAllText("Inputs\\day5.txt");
+            string[] parts = content.Split("\r\n\r\n");
+
+            string[] containers = parts[0].Split("\n");
+            string[] cmd = parts[1].Split("\n");
+
+            return (containers, cmd);
+        }
+
+
+        private List<List<char>> ReadContainer(string[] container)
         {
             var content = File.ReadAllLines("Inputs\\day5.txt");
 
             List<List<char>> stacks = new List<List<char>>();
-            int end = 0;
-
-
-            for (int i = 0; i < content.Length; i++) { if (content[i].Contains(" 1   2   3")) { end = i - 1; } }
 
             for (int s = 0; s < CountStacks; s++)
             {
                 stacks.Add(new List<char>());
 
                 int datPos = (s * 4) + 1;
-                for (int i = 0; i <= end; i++)
+                for (int i = 0; i <= container.Count() - 1; i++)
                 {
-                    if (content[i].Length >= datPos && char.IsLetter(content[i][datPos]))
-                    {
-                        stacks[s].Add(content[i][datPos]);
-                    }
+                    if (content[i].Length >= datPos && char.IsLetter(content[i][datPos])) { stacks[s].Add(content[i][datPos]); }
                 }
-
             }
 
-            foreach (var item in stacks) { item.Reverse(); }
+            stacks.ForEach(x => x.Reverse());
 
-            return (stacks, end + 3);
+            return stacks;
         }
 
-        private void Move(ref List<List<char>> stacks, string cmd)
+        private void CreateMover9000(ref List<List<char>> stacks, string cmdRow)
         {
-            string[] cmdParts = cmd.Split(' ');
-            int count = Convert.ToInt32(cmdParts[1]);
-            int sourceStack = Convert.ToInt32(cmdParts[3]) - 1;
-            int targetStack = Convert.ToInt32(cmdParts[5]) - 1;
+            CMD cmd = new CMD(cmdRow);
 
-
-            for (int i = 0; i < count; i++)
+            for (int i = 0; i < cmd.Count; i++)
             {
-                var value = stacks[sourceStack][stacks[sourceStack].Count - 1];
-                stacks[sourceStack].RemoveAt(stacks[sourceStack].Count - 1);
-                stacks[targetStack].Add(value);
+                var value = stacks[cmd.SourceStack][stacks[cmd.SourceStack].Count - 1];
+                stacks[cmd.SourceStack].RemoveAt(stacks[cmd.SourceStack].Count - 1);
+                stacks[cmd.TargetStack].Add(value);
             }
-
-
         }
 
- 
-        public void Part2()
+        private void CreateMover9001(ref List<List<char>> stacks, string cmdRow)
         {
-            //throw new NotImplementedException();
+            CMD cmd = new CMD(cmdRow);
+
+            int startMove = stacks[cmd.SourceStack].Count - cmd.Count;
+            int endMove = stacks[cmd.SourceStack].Count - startMove;
+
+            var value = stacks[cmd.SourceStack].GetRange(startMove, endMove);
+            stacks[cmd.SourceStack].RemoveRange(startMove, endMove);
+            stacks[cmd.TargetStack].AddRange(value);
         }
+
+        class CMD
+        {
+
+            public int Count { get; set; }
+            public int SourceStack { get; set; }
+            public int TargetStack { get; set; }
+
+            public CMD(string cmdRow)
+            {
+                string[] cmdParts = cmdRow.Split(' ');
+                Count = Convert.ToInt32(cmdParts[1]);
+                SourceStack = Convert.ToInt32(cmdParts[3]) - 1;
+                TargetStack = Convert.ToInt32(cmdParts[5]) - 1;
+            }
+        }
+
+        enum Mover
+        {
+            CreateMover9000,
+            CreateMover9001
+        }
+
     }
 }
